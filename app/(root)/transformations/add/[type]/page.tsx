d3b6b8c42d6a77da3d179b65f67165e473686201
kaneHassan <hassanknmr@gmail.com>
2024-03-06 14:13:39 -0500

    first commit

---
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -0,0 +1,34 @@
+import Header from "@/components/shared/Header";
+import TransformationForm from "@/components/shared/TransformationForm";
+import { transformationTypes } from "@/constants";
+import { getUserById } from "@/lib/actions/user.actions";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+const AddTransformationTypePage = async ({
+  params: { type },
+}: SearchParamProps) => {
+  const { userId } = auth();
+  const transformation = transformationTypes[type];
+
+  if (!userId) redirect("/sign-in");
+
+  const user = await getUserById(userId);
+
+  return (
+    <>
+      <Header title={transformation.title} subtitle={transformation.subTitle} />
+
+      <section className="mt-10">
+        <TransformationForm
+          action="Add"
+          userId={user ? user._id : ""} // Add a conditional check for user object
+          type={transformation.type as TransformationTypeKey}
+          creditBalance={user ? user.creditBalance : 0} // Example default value if user is undefined
+        />
+      </section>
+    </>
+  );
+};
+
+export default AddTransformationTypePage;
